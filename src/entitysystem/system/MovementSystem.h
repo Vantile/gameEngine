c918b061dff250973884b858b8c52d7b8a562156
Vantile <vantilesyfon@gmail.com>
2026-07-27 11:12:42 +0200

    Jobified MovementSystem and added profiling

--- src/entitysystem/system/MovementSystem.h
+++ src/entitysystem/system/MovementSystem.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <engine/Engine.h>
 #include <entitysystem/component/TransformComponent.h>
 #include <entitysystem/system/System.h>
 #include <random>
@@ -9,15 +10,28 @@ class MovementSystem : public System
 public:
 	void Update(EntityRegistry& registry, FrameData& frameData) override
 	{
-		registry.GetView<TransformComponent>().Each([](TransformComponent& transform)
+		ZoneScoped;
+		JobCounter systemCounter;
+		JobSystem& jobSystem = Engine::GetInstance().GetJobSystem();
+		registry.GetView<TransformComponent>().Each([&systemCounter, &jobSystem](TransformComponent& transform)
 			{
-				glm::vec3& position = transform.m_Position;
-				std::mt19937 gen(std::random_device{}());
-				constexpr float diff = 0.01f;
-				std::uniform_real_distribution<float> xDist(position.x - diff, position.x + diff);
-				std::uniform_real_distribution<float> yDist(position.y - diff, position.y + diff);
-				position.x = xDist(gen);
-				position.y = yDist(gen);
+				jobSystem.Submit({
+					[&transform]() {
+						ZoneScopedN("MovementSystem::Update Job");
+						glm::vec3& position = transform.m_Position;
+						std::mt19937 gen(std::random_device{}());
+						constexpr float diff = 0.01f;
+						std::uniform_real_distribution<float> xDist(position.x - diff, position.x + diff);
+						std::uniform_real_distribution<float> yDist(position.y - diff, position.y + diff);
+						position.x = xDist(gen);
+						position.y = yDist(gen);
+					}
+				}, &systemCounter);
 			});
+
+		{
+			ZoneScopedN("MovementSystem::Update Wait");
+			systemCounter.Wait();
+		}
 	}
 };
--- src/entitysystem/system/RenderSystem.h
+++ src/entitysystem/system/RenderSystem.h
@@ -9,26 +9,27 @@ class RenderSystem : public System
 public:
 	void Update(EntityRegistry& registry, FrameData& frameData) override
 	{
+		ZoneScoped;
 		registry.GetView<TransformComponent, RenderableComponent>().Each([&frameData](TransformComponent& transform, RenderableComponent& renderable)
+		{
+			RenderObject object;
+			object.point = renderable.m_RenderType == RenderableComponent::RenderType::Point;
+			object.entityID = renderable.GetOwner();
+
+			if (object.point)
 			{
-				RenderObject object;
-				object.point = renderable.m_RenderType == RenderableComponent::RenderType::Point;
-				object.entityID = renderable.GetOwner();
+				Vertex vertex = {};
+				vertex.m_Position = transform.m_Position;
+				vertex.m_Color = { 1.f, 1.f, 0.f, 1.f };
+				object.vertices.push_back(vertex);
+			}
+			else
+			{
+				object.vertices = renderable.m_Vertices;
+				object.indices = renderable.m_Indices;
+			}
 
-				if (object.point)
-				{
-					Vertex vertex = {};
-					vertex.m_Position = transform.m_Position;
-					vertex.m_Color = { 1.f, 1.f, 0.f, 1.f };
-					object.vertices.push_back(vertex);
-				}
-				else
-				{
-					object.vertices = renderable.m_Vertices;
-					object.indices = renderable.m_Indices;
-				}
-
-				frameData.renderObjects.push_back(std::move(object));
-			});
+			frameData.renderObjects.push_back(std::move(object));
+		});
 	}
 };
